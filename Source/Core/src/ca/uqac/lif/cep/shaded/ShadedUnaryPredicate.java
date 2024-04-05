1b0a680b572a52830648325004d38b64d7946915
Sylvain Hallé <shalle@acm.org>
2024-04-05 13:19:29 -0400

    Bugfix unary predicate

---
+++ Source/Core/src/ca/uqac/lif/cep/shaded/ShadedUnaryPredicate.java
@@ -0,0 +1,88 @@
+package ca.uqac.lif.cep.shaded;
+
+public abstract class ShadedUnaryPredicate extends ShadedConnective
+{
+	public static IsEven even(Object argument)
+	{
+		return new IsEven(ShadedConstant.wrap(argument));
+	}
+
+	protected final ShadedFunction m_argument;
+
+	public ShadedUnaryPredicate(ShadedFunction arg)
+	{
+		super();
+		m_argument = arg;
+		m_color = null;
+	}
+
+	@Override
+	public int getArity()
+	{
+		return 1;
+	}
+
+	@Override
+	public ShadedFunction getOperand(int index)
+	{
+		if (index == 0)
+		{
+			return m_argument;
+		}
+		return null;
+	}
+
+	@Override
+	public void setValue(String name, Object value)
+	{
+		m_argument.setValue(name, value);
+	}
+
+	public static class IsEven extends ShadedUnaryPredicate
+	{
+		public IsEven(ShadedFunction arg)
+		{
+			super(arg);
+		}
+
+		@Override
+		public IsEven duplicate(boolean with_state)
+		{
+			IsEven ie = new IsEven(m_argument.duplicate(with_state));
+			if (with_state)
+			{
+				ie.m_color = m_color;
+			}
+			return ie;
+		}
+
+		@Override
+		public ShadedUnaryPredicate update(Object event)
+		{
+			if (m_color == null)
+			{
+				m_argument.update(event);
+				Object o = m_argument.getValue();
+				m_color = (o instanceof Number && ((Number) o).intValue() % 2 == 0) ? Color.GREEN : Color.RED;
+			}
+			return this;
+		}
+
+		@Override
+		public String getSymbol()
+		{
+			return "even";
+		}
+
+		@Override
+		public boolean sameAs(ShadedFunction f)
+		{
+			if (!(f instanceof IsEven))
+			{
+				return false;
+			}
+			IsEven ie = (IsEven) f;
+			return m_argument.sameAs(ie.m_argument);
+		}
+	}
+}
--- Source/Core/srctest/ca/uqac/lif/cep/shaded/QuantifiersTest.java
+++ Source/Core/srctest/ca/uqac/lif/cep/shaded/QuantifiersTest.java
@@ -2,7 +2,7 @@ package ca.uqac.lif.cep.shaded;
 
 import org.junit.Test;
 
-import ca.uqac.lif.cep.shaded.Polarized.Polarity;
+import ca.uqac.lif.cep.shaded.DotRenderer.Format;
 import ca.uqac.lif.cep.shaded.ShadedConnective.Color;
 import ca.uqac.lif.xml.XPathExpression.XPathParseException;
 import ca.uqac.lif.xml.XmlElement;
@@ -12,10 +12,14 @@ import static org.junit.Assert.*;
 
 import static ca.uqac.lif.cep.shaded.ShadedComparison.leq;
 import static ca.uqac.lif.cep.shaded.ShadedForAll.all;
+import static ca.uqac.lif.cep.shaded.ShadedOr.or;
 import static ca.uqac.lif.cep.shaded.ShadedQuantifiedVariable.v;
+import static ca.uqac.lif.cep.shaded.ShadedUnaryPredicate.even;
 
 public class QuantifiersTest
 {
+	public static final TreeRenderer s_renderer = new TreeRenderer(false);
+	
 	@Test
 	public void test1() throws XPathParseException, XmlParseException
 	{
@@ -41,6 +45,21 @@ public class QuantifiersTest
 		ShadedConnective phi = all("x", "m/foo/text()", leq(v("x"), 3));
 		ShadedConnective phi1 = phi.duplicate().update(XmlElement.parse("<m><foo>2</foo><foo>10</foo></m>"));
 		ShadedConnective phi2 = phi.duplicate().update(XmlElement.parse("<m><foo>20</foo><foo>10</foo></m>"));
+		s_renderer.toImage(phi1, "/tmp/phil1.png", Format.PNG);
+		s_renderer.toImage(phi2, "/tmp/phil2.png", Format.PNG);
+		Subsumption rel = new Subsumption(false);
+		assertTrue(rel.inRelation(phi2, phi1));
+		assertFalse(rel.inRelation(phi1, phi2));
+	}
+	
+	@Test
+	public void testSubsumption2() throws XPathParseException, XmlParseException
+	{
+		ShadedConnective phi = all("x", "m/foo/text()", or(leq(v("x"), 3), even(v("x"))));
+		ShadedConnective phi1 = phi.duplicate().update(XmlElement.parse("<m><foo>2</foo><foo>10</foo></m>"));
+		ShadedConnective phi2 = phi.duplicate().update(XmlElement.parse("<m><foo>20</foo><foo>10</foo></m>"));
+		s_renderer.toImage(phi1, "/tmp/phi1.png", Format.PNG);
+		s_renderer.toImage(phi2, "/tmp/phi2.png", Format.PNG);
 		Subsumption rel = new Subsumption(false);
 		assertTrue(rel.inRelation(phi2, phi1));
 		assertFalse(rel.inRelation(phi1, phi2));
