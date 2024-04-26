b828d1edd98a4c52beaf97ed1e7bc355d01339ba
Sylvain Hallé <shalle@acm.org>
2024-04-26 10:09:18 -0400

    LGPL comments

--- Source/Benchmark/src/treecompliancelab/BufferedPicker.java
+++
@@ -1,6 +0,0 @@
-package treecompliancelab;
-
-public class BufferedPicker
-{
-
-}
--- Source/Core/src/ca/uqac/lif/cep/shaded/CommandRunner.java
+++ Source/Core/src/ca/uqac/lif/cep/shaded/CommandRunner.java
@@ -1,6 +1,6 @@
 /*
-    BeepBeep, an event stream processor
-    Copyright (C) 2008-2016 Sylvain Hallé
+    A tree-based process compliance library
+    Copyright (C) 2024 Sylvain Hallé
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
